13f7ea5f34d09f598b24de0750dfddde2f566b2c
a-burlakov <fln@mail.ru>
2022-12-27 22:22:07 +0500

    Learning SQLAlchemy and making forms.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id # wtf
 
 @app.route('/')
 @app.route('/home')
@@ -9,14 +24,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "GET":
+        return render_template('create-article.html')
 
+    title = request.form['title']
+    intro = request.form['intro']
+    text = request.form['text']
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'user page: ' + name + ' - ' + str(id)
+    article = Article(title=title, intro=intro, text=text)
+
+    try:
+        db.session.add(article)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "При добавлении статьи произошла ошибка."
+
 
 if __name__ == '__main__':
     app.run(debug=True)

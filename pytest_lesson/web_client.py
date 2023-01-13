7a0424124e3a3642ab748e3108156b849d52c3b2
a-burlakov <fln@mail.ru>
2023-01-13 16:47:41 +0500

    learn: some little practice.

--- pytest_lesson/web_client.py
+++ pytest_lesson/web_client.py
@@ -1,23 +1,5 @@
-import requests
-import json
-from dataclasses import dataclass
+def create_phone_number():
+    n = [str(x) for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]]
+    return f'({"".join(n[:3])}) {"".join(n[3:6])}-{"".join(n[6:])}'
 
-
-@dataclass
-class SomeResourceClient:
-    url: str
-
-    def __user_get_status(self, user_id: str):
-        resp = requests.get(f"{self.url}/web/user/get-status/{user_id}")
-        json_data = json.loads(resp.text)
-        return json_data
-
-    def get_information(self, user_id: str):
-        json_data = self.__user_get_status(user_id)
-        return json_data
-
-
-if __name__ == "__main__":
-    src = SomeResourceClient(url="https://www.avito.ru")
-    result = src.get_information("177041581")
-    print(result)
+print(create_phone_number())

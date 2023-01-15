fd3d4c724f442e3a00fbd1e99f98707841d4174e
a-burlakov <fln@mail.ru>
2023-01-15 23:28:04 +0500

    Codewars training.

--- codewars/main.py
+++ codewars/main.py
@@ -1,18 +1,8 @@
-def codewars(text):
-    parentheses = ''.join([x for x in text if x in ['(', ')']])
 
-    while parentheses:
-        first_close = parentheses.find(')')
-        first_open = parentheses.find('(')
+def codewars(array1, array2):
 
-        if first_close < 0 or \
-                first_open < 0 or \
-                first_open > first_close:
-            return False
+    return sorted({x for x in array1 if any([x in y for y in array2])})
 
-        for i in sorted([first_open, first_close], reverse=True):
-            parentheses = parentheses[:i] + parentheses[i + 1:]
-
-    return True
-
-print(codewars("((sd))(((ff)qr(eq))(rwer))"))
+a1 = ["live", "arp", "strong"]
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+print(codewars(a1, a2))

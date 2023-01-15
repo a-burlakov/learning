7ef55d4b03dd6d35bbcafc355498d4dbd6e6dee3
a-burlakov <fln@mail.ru>
2023-01-15 23:04:25 +0500

    Codewars training.

--- codewars/main.py
+++ codewars/main.py
@@ -1,8 +1,18 @@
-from collections import Counter
-
 def codewars(text):
-    return len([x for x in Counter(text).values() if x > 1])
+    parentheses = ''.join([x for x in text if x in ['(', ')']])
 
+    while parentheses:
+        first_close = parentheses.find(')')
+        first_open = parentheses.find('(')
 
+        if first_close < 0 or \
+                first_open < 0 or \
+                first_open > first_close:
+            return False
 
-print(codewars("Indivisibilities"))
+        for i in sorted([first_open, first_close], reverse=True):
+            parentheses = parentheses[:i] + parentheses[i + 1:]
+
+    return True
+
+print(codewars("((sd))(((ff)qr(eq))(rwer))"))

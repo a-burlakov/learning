1d3a50850801964a14da97c9e1ffffd6b11e8b55
a-burlakov <fln@mail.ru>
2023-01-19 16:54:42 +0500

    learn: asyncio.

--- asynch/main.py
+++ asynch/main.py
@@ -1,24 +1,28 @@
-import multiprocessing
 import time
-import random
-
-def worker(number):
-    sleep = random.randrange(1,5)
-    time.sleep(sleep)
-    print(f"Я работник {str(number)}, я спал {sleep} секунд!")
+import asyncio
 
 
-if __name__ == '__main__':
+async def fun1(x):
+    print(x ** 2)
+    await asyncio.sleep(3)
+    print('fun1 завершена')
 
-    procs = []
-    for i in range(5):
-        proc = multiprocessing.Process(target=worker, args=(i,))
-        proc.start()
-        procs.append(proc)
 
-    print("Ну вот и все, ждем наши процессы!")
+async def fun2(x):
+    print(x ** 0.5)
+    await asyncio.sleep(3)
+    print('fun2 завершена')
 
-    for proc in procs:
-        proc.join()
 
-    print('Все кончилось')
+async def main():
+    task1 = asyncio.create_task(fun1(4))
+    task2 = asyncio.create_task(fun2(4))
+
+    await task1
+    await task2
+
+print(time.strftime('%X'))
+
+asyncio.run(main())
+
+print(time.strftime('%X'))

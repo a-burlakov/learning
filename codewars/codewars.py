7cecf8edf9ba3b7ee6af580d7c2dc73f0a22c97c
a-burlakov <fln@mail.ru>
2023-01-20 13:43:51 +0500

    learn: CodeWar practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,23 +1,39 @@
-
-def codewars(number_list: list[int]) -> int:
-    """
-    Returns second after max number from the list.
-
-    :param number_list: list with numbers.
-    :return: number that is second after max number in the list.
-    """
-    max_number = 0
-    second_after_max_number = 0
-    for number in number_list:
-        if max_number < number:
-            second_after_max_number = max_number
-            max_number = number
-        elif second_after_max_number < number:
-            second_after_max_number = number
-
-    return second_after_max_number
+import itertools
 
 
-print(codewars([1, 4, 2, 6, 5]))
-print(codewars([5, 3, 1, 2, 4]))
-print(codewars([1, 2, 3, 4, 5]))
+def codewars(observed: str) -> list[str]:
+    # ┌───┬───┬───┐
+    # │ 1 │ 2 │ 3 │
+    # ├───┼───┼───┤
+    # │ 4 │ 5 │ 6 │
+    # ├───┼───┼───┤
+    # │ 7 │ 8 │ 9 │
+    # └───┼───┼───┘
+    #     │ 0 │
+    #     └───┘
+
+    adjacents = {
+        '1': ['2', '4', '1'],
+        '2': ['1', '3', '5', '2'],
+        '3': ['2', '6', '3'],
+        '4': ['1', '5', '7', '4'],
+        '5': ['2', '4', '6', '8', '5'],
+        '6': ['3', '5', '9', '6'],
+        '7': ['4', '8', '7'],
+        '8': ['5', '7', '9', '0', '8'],
+        '9': ['6', '8', '9'],
+        '0': ['8', '0'],
+    }
+
+    lists = []
+    for symb in observed:
+        lists.append(adjacents[symb])
+
+    pin_list = []
+    for pin in itertools.product(*lists):
+        pin_list.append(''.join(pin))
+
+    return pin_list
+
+
+print(codewars('11'))

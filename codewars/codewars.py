25052d5709e9ac60cc5b63ce6e2addbb8728fd65
a-burlakov <fln@mail.ru>
2023-01-20 16:37:11 +0500

    learn: CodeWar practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,29 +1,17 @@
-import itertools
+
+def codewars(core_number: int) -> list:
+    core_sum = sum(range(1, core_number + 1))
+
+    fit_pairs = []
+    for i in range(1, core_number + 1):
+
+        j = (core_sum - i) // i
+
+        if j * i == (core_sum - j - i):
+            fit_pairs.append((j, i))
+            fit_pairs.append((i, j))
+
+    return sorted(fit_pairs, key=lambda x: x[0])
 
 
-def codewars(message: str) -> str:
-
-    cipher = ''
-    lower_start = ord('a')
-    lower_end = ord('z')
-    upper_start = ord('A')
-    upper_end = ord('Z')
-    for symbol in message:
-
-        if not symbol.isalpha():
-            cipher += symbol
-            continue
-
-        symbol_code = ord(symbol)
-
-        if symbol.islower() and symbol_code + 13 > lower_end:
-            new_symbol = chr(lower_start + (13 - lower_end + symbol_code) - 1)
-        elif symbol.isupper() and symbol_code + 13 > upper_end:
-            new_symbol = chr(upper_start + (13 - upper_end + symbol_code) - 1)
-        else:
-            new_symbol = chr(symbol_code + 13)
-
-        cipher += new_symbol
-
-    return cipher
-
+print(codewars(26))

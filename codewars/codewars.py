cd2f07331b75026959a3eb409e95c4f92ef240a5
a-burlakov <fln@mail.ru>
2023-01-22 18:32:38 +0500

    learn: CodeWar practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,15 +1,11 @@
-def codewars(int32: int) -> str:
-    bin_line = format(int32, 'b')
-    if len(bin_line) < 32:
-        bin_line = '0' * (32 - len(bin_line)) + bin_line
+def codewars(a: list[int], b: list[int]) -> list:
 
-    ip_octets = []
-    for i in range(4):
-        ip_octets.append(bin_line[i * 8:(i + 1) * 8])
+    for number_to_delete in b:
 
-    return '.'.join([str(int(ip, 2)) for ip in ip_octets])
+        while number_to_delete in a:
+            a.remove(number_to_delete)
+
+    return a
 
 
-print(codewars(2149583361))
-print(codewars(32))
-print(codewars(0))
+print(codewars([1,2,2], []))

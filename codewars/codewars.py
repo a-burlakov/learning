a729d3a9125bd931580b307bcf71cb940c65b954
a-burlakov <fln@mail.ru>
2023-01-22 18:45:10 +0500

    learn: CodeWar practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,10 +1,6 @@
 
 def codewars(string):
-
-    if not string:
-        return True
-
-    return all([string.lower().count(x.lower()) == 1 for x in string])
+    return len([x for x in sentence if x in "aeiou"])
 
 
 print(codewars("mOose"))

5f0e86ec1b756a8dd17137003739195a4087ce3a
a-burlakov <fln@mail.ru>
2023-01-23 18:02:37 +0500

    learn: CodeWars practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,7 +1,7 @@
 def codewars(walk: list):
-    return len(walk) == 10 \
-           and walk.count('s') == walk.count('n') \
-           and walk.count('e') == walk.count('w')
+    return (len(walk) == 10
+            and walk.count('s') == walk.count('n')
+            and walk.count('e') == walk.count('w'))
 
 
 print(codewars(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']))

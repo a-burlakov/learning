127273ac03071fe32eae4a5cc872886b89790de5
a-burlakov <fln@mail.ru>
2023-01-23 18:01:41 +0500

    learn: CodeWars practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,11 +1,7 @@
-from collections import defaultdict
+def codewars(walk: list):
+    return len(walk) == 10 \
+           and walk.count('s') == walk.count('n') \
+           and walk.count('e') == walk.count('w')
 
-def codewars(n):
 
-    count_dict = defaultdict(int)
-    for number in n:
-        count_dict[number] += 1
-
-    return [k for k, v in count_dict.items() if v == 1][0]
-
-print(codewars([ 0, 0, 0.55, 0, 0 ]))
+print(codewars(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']))

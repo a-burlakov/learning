32fac2f175d5fd1118654b562c0fd52b944126dc
a-burlakov <fln@mail.ru>
2023-01-24 18:45:11 +0500

    learn: Сodewars practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,27 +1,40 @@
-def codewars(board: list[list[int]]) -> bool:
-    subgrids = []
-    for i in range(3):
-        for j in range(3):
-            subgrids.append(board[j * 3 + 0][i * 3:i * 3 + 3] +
-                            board[j * 3 + 1][i * 3:i * 3 + 3] +
-                            board[j * 3 + 2][i * 3:i * 3 + 3])
+class Sudoku(object):
+    def __init__(self, board):
+        self.board = board
+        pass
 
-    # Checking rows, columns and subgrids.
-    numbers_collections = [board, zip(*board), subgrids]
-    for numbers_collection in numbers_collections:
-        for numbers in numbers_collection:
-            if 0 in numbers or len(set(numbers)) != 9:
-                return False
+    def is_valid(self):
 
-    return True
+        # Checking sudoku format.
+        size = len(self.board[0])
+        if len(self.board) != size or any([len(row) != size for row in self.board]):
+            return False
+
+        for row in self.board:
+            for n in row:
+                if not type(n) is int:
+                    return False
+
+        sa = int(size ** 0.5)
+
+        subgrids = []
+        for i in range(sa):
+            for j in range(sa):
+                subgrid = []
+                for z in range(sa):
+                    subgrid.extend(self.board[j * sa + z][i * sa:i * sa + sa])
+                subgrids.append(subgrid)
+
+        # Checking rows, columns and subgrids.
+        numbers_collections = [self.board, zip(*self.board), subgrids]
+        for numbers_collection in numbers_collections:
+            for numbers in numbers_collection:
+                if 0 in numbers or len(set(numbers)) != size:
+                    return False
+
+        return True
 
 
-print(codewars([[5, 3, 4, 6, 7, 8, 9, 1, 2],
-                [6, 7, 2, 1, 9, 5, 3, 4, 8],
-                [1, 9, 8, 3, 4, 2, 5, 6, 7],
-                [8, 5, 9, 7, 6, 1, 4, 2, 3],
-                [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                [9, 6, 1, 5, 3, 7, 2, 8, 4],
-                [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                [3, 4, 5, 2, 9, 6, 1, 7, 9]]))
+badSudoku2 = Sudoku([[2]])
+
+print(badSudoku2.is_valid())

837e00c5d88afae0c09aca9f919a413a6d757bb5
a-burlakov <fln@mail.ru>
2023-01-25 12:54:55 +0500

    learn: Сodewars practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 class Node:
     def __init__(self, L, R, n):
         self.left: Node = L
@@ -8,7 +9,6 @@ class Node:
 
 
 def codewars(node):
-
     if node is None:
         return []
 
@@ -18,13 +18,13 @@ def codewars(node):
         current_node = queue.pop()
         if current_node is not None:
             result_list.append(current_node.value)
-            if current_node.left is not None:
-                queue.append(current_node.left)
             if current_node.right is not None:
                 queue.append(current_node.right)
-
+            if current_node.left is not None:
+                queue.append(current_node.left)
     return result_list
 
 
-result = codewars(Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1))
+result = codewars(Node(Node(None, Node(None, None, 4), 2),
+                       Node(Node(None, None, 5), Node(None, None, 6), 3), 1))
 print(result)

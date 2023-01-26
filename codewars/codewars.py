e68bca5c9802547c9ce4a0547018022fcf10d597
a-burlakov <fln@mail.ru>
2023-01-26 11:37:06 +0500

    learn: CodeWar practice.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,14 +1,36 @@
-from collections import Counter
 
+def generator(x):
 
-def codewars(text):
-    text = ''.join([s if s.isalpha() or s == "'" else ' ' for s in text])
-    words = ''.join([s for s in text.lower() if s.isalpha() or s in " '"]).split()
-    words = filter(lambda word: not all([x == "'" for x in word]), words)
-    return [s for s, a in Counter(words).most_common(3)]
+    degree = 0
+    while True:
+        degree += 1
+        yield x ** degree
 
-
-result = codewars("AGU:/AGU?:!AGU_!/.;AGU::/ ;AGU;AGU:-;-:AGU. :AGU.__?,AGU;:_-AGU!: AGU;??AGU/.:!AGU.AGU .AGU/_/,:")
-print(result)
-result = codewars("  '''  ")
-print(result)
+gen = generator(2)
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())
+print(gen.__next__())

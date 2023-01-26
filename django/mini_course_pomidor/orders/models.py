6df1223e625e0df0f78bca76363709ed5369c567
a-burlakov <fln@mail.ru>
2023-01-26 22:27:11 +0500

    learn: Django (Senior Pomidor mini-course).

--- django/mini_course_pomidor/orders/models.py
+++ django/mini_course_pomidor/orders/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product, ProductType
 
 
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.deletion.SET_NULL, null=True)
+    products = models.ManyToManyField(Product)
---
+++ django/mini_course_pomidor/products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=250)

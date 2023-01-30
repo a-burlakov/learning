69604a5f2b5d29c3736702466de2262108c16ce7
a-burlakov <fln@mail.ru>
2023-01-30 17:53:37 +0500

    learn: Django (Django School channel).

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from movies.models import Movie
+
+
+class MoviesView(View):
+    """
+    Список фильмов
+    """
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {"movie_list": movies})

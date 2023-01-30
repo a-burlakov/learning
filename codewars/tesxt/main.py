04615ccc97d505bb6330dffc4f7c14f35b5a9a70
a-burlakov <fln@mail.ru>
2023-01-30 14:33:14 +0500

    learn: Django (Django School channel).

--- codewars/tesxt/main.py
+++ codewars/tesxt/main.py
@@ -3,6 +3,5 @@ def codewars(array1, array2):
 
     return sorted({x for x in array1 if any([x in y for y in array2])})
 
-a1 = ["live", "arp", "strong"]
-a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
-print(codewars(a1, a2))
+a1: str = 'very very very very very very very very very ' \
+     'very very very very very very long text'

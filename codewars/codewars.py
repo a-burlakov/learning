cc8646f99807e19d41ec4b17b27e9f547c5e8552
a-burlakov <fln@mail.ru>
2023-02-01 18:04:06 +0500

    learn: Codewars practice (was busy writing article about books for developers).

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,2 +1,15 @@
-a = '.'.join((str(s) for s in range(10)))
-print(type(a))
+def codewars(n, p):
+
+    sum_of_powers = sum([int(x) ** (i + p) for i, x in enumerate(str(n))])
+    k = sum_of_powers / n
+    if k.is_integer():
+        return k
+    else:
+        return -1
+
+
+def tests():
+
+    assert codewars(46288, 3) == 51
+
+tests()

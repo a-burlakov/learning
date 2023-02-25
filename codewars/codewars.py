0dfccffab5528a46f549f9a5183b325985d93f03
a-burlakov <fln@mail.ru>
2023-02-25 17:07:57 +0500

    learn: some little kata #2.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,18 +1,19 @@
-# two_sum([1, 2, 3], 4) # returns [0, 2] or [2, 0]
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))(("
+def codewars(word: str) -> str:
+	word = word.lower()
+	letters = set(word)
+	decoding_table = {}
+	for letter in letters:
+		if word.count(letter) == 1:
+			decoding_table[letter] = "("
+		else:
+			decoding_table[letter] = ")"
+
+	return "".join([decoding_table[x] for x in word])
 
 
-# test.assert_equals(sorted(two_sum([1,2,3], 4)), [0,2])
-# test.assert_equals(sorted(two_sum([1234,5678,9012], 14690)), [1,2])
-# test.assert_equals(sorted(two_sum([2,2,3], 4)), [0,1])
 
-def codewars(numbers: list[int], target: int) -> tuple[int, int] | None:
-
-	for i, i_num in enumerate(numbers):
-		for j, j_num in enumerate(numbers):
-			if i == j: continue
-			if i_num + j_num == target:
-				return i, j
-
-	return None
-
-print(codewars([1,2,3], 4))
+codewars("Succ ess")

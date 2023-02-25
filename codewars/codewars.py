ef5bfe44e138e98da0c521abf421096b47711e07
a-burlakov <fln@mail.ru>
2023-02-25 16:49:44 +0500

    learn: some little kata.

--- codewars/codewars.py
+++ codewars/codewars.py
@@ -1,10 +1,18 @@
-a = [1, 2, 3]
-print(a.pop())  # 3
-print(a.pop(0))  # 1
-print(a) # 2
+# two_sum([1, 2, 3], 4) # returns [0, 2] or [2, 0]
 
-a.extend(4) # 2, 4
-print(a)
 
-a.remove(0) # 4
-print(a)
+# test.assert_equals(sorted(two_sum([1,2,3], 4)), [0,2])
+# test.assert_equals(sorted(two_sum([1234,5678,9012], 14690)), [1,2])
+# test.assert_equals(sorted(two_sum([2,2,3], 4)), [0,1])
+
+def codewars(numbers: list[int], target: int) -> tuple[int, int] | None:
+
+	for i, i_num in enumerate(numbers):
+		for j, j_num in enumerate(numbers):
+			if i == j: continue
+			if i_num + j_num == target:
+				return i, j
+
+	return None
+
+print(codewars([1,2,3], 4))
